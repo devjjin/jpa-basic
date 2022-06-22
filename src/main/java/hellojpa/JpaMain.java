386451628fa107b2d69a59eb25adfa93a6d26760
devjjin <sujink3434@gmail.com>
2022-06-22 16:34:26 +0900

    feat: 영속성 컨텍스트 준영속 상태로

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,16 +16,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member member1 = new Member(150L, "A");
-//            Member member2= new Member(160L, "B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-//            System.out.println("=======before==========");
 
-            // 변경감지
-            Member member = em.find(Member.class, 150L);
-            member.setName("zzzzzz");
+           Member member = em.find(Member.class, 150L);
+           member.setName("AAAAA");
+
+           //em.detach(member);
+            em.clear();
 
             tx.commit();
 
